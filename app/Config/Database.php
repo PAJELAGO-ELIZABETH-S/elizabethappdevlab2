3e9abffdc6448350c67070a139623763f5ba3160
11Elizabeth22 <129596792+11Elizabeth22@users.noreply.github.com>
2023-09-30 14:35:19 +0800

    Update Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => 'root',
         'password'     => '',
-        'database'     => 'elizabethdb',
+        'database'     => 'musicplayerdb',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
